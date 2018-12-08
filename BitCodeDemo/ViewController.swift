046e08f50de09233837de9443a7cfdd0de515337
NAYAN MANDRE <nayanmandre.aam@gmail.com>
2018-12-08 12:40:51 +0530

    Second commit

--- BitCodeDemo/ViewController.swift
+++ BitCodeDemo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hellooooo")
+          print("Hellooooo second commit")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
